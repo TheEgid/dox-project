cd35c07febfaa9ff5170c03655671a79c75f4304
TheEgid <egid.f1@gmail.com>
2021-11-18 21:36:23 +0300

    Fix main bootstrap

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,15 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
     parserOptions: {
+        ecmaVersion: 2020,
         project: 'tsconfig.json',
         sourceType: 'module',
     },
-    plugins: ['@typescript-eslint/eslint-plugin'],
+    plugins: ['@typescript-eslint/eslint-plugin', 'import', 'prettier'],
     extends: [
-        'plugin:@typescript-eslint/recommended',
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
         'plugin:prettier/recommended',
     ],
     root: true,
@@ -20,5 +23,25 @@ module.exports = {
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
+        "semi": 0,
+        "space-before-function-paren": 0,
+        "arrow-parens": 0, // Несовместимо с prettier
+        "linebreak-style": 0,
+        "no-nested-ternary": 2,
+        "no-console": 0,
+        "class-methods-use-this": 0,
+        "object-curly-newline": 0,
+        "prettier/prettier": [
+            "warn",
+            {
+                "printWidth": 100,
+                "tabWidth": 2,
+                "bracketSpacing": true,
+                "bracketSameLine": true,
+                "semi": true,
+                "singleQuote": false,
+                "trailingComma": "es5"
+            }
+        ]
     },
 };
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-bootstrap();
+
+bootstrap()
+  .then(() => {
+    console.log("running");
+  })
+  .catch((error) => {
+    console.log("Error: ".concat(error as string));
+  });
