29e57170efde62a41d71f861542233429ad14a36
TheEgid <egid.f1@gmail.com>
2021-11-20 22:56:22 +0300

    Fix "start"

--- src/app.middleware.ts
+++ src/app.middleware.ts
@@ -1,23 +1,23 @@
 import { Injectable, NestMiddleware, Logger } from "@nestjs/common";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
 @Injectable()
 export class HealthcheckMiddleware implements NestMiddleware {
-  use(req: Request, res: Response) {
-    res.json({ status: "OK" });
+  use(request: Request, response: Response): void {
+    response.json({ status: "OK" });
   }
 }
 
 @Injectable()
 export class AppLoggerMiddleware implements NestMiddleware {
   private logger = new Logger("HTTP");
-  use(request: Request, response: Response) {
-    const { ip, method, path: url } = request;
-    const userAgent = request.get("user-agent") || "";
+  use(request: Request, response: Response, next: NextFunction): void {
+    const { method, path: url } = request;
     response.on("close", () => {
       const { statusCode } = response;
       const contentLength = response.get("content-length");
-      this.logger.log(`${method} ${url} ${statusCode} ${contentLength} - ${userAgent} ${ip}`);
+      this.logger.log(`${method} ${url} ${statusCode} ${contentLength}`);
     });
+    next();
   }
 }
