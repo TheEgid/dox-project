81b984b8759df14ffb37f3260c06b1e4fc22f810
TheEgid <egid.f1@gmail.com>
2021-12-19 19:43:55 +0300

    Fix typo

--- src/database/fixconfig.ts
+++ src/database/fixconfig.ts
@@ -2,14 +2,15 @@ import * as fs from "fs";
 import dbConnectionOptions from "./database.config";
 import path from "path";
 
-const migrationsParams = {
+const migrationParams = {
   name: "",
   migrations: [path.join(__dirname, "/../../migrations") + "/*.ts"],
   cli: {
     migrationsDir: path.join(__dirname, "/../../migrations"),
   },
 };
+
 fs.writeFileSync(
   "ormconfig.json",
-  JSON.stringify(Object.assign(dbConnectionOptions, migrationsParams), null, 4)
+  JSON.stringify(Object.assign(dbConnectionOptions, migrationParams), null, 4)
 );
